8d05bae9eab70f56c2c99bba5b45918bf0a93132
Kauã Steliano <stelianok@gmail.com>
2024-01-08 14:14:26 -0300

    Created route to register users

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,41 @@
-import fastify from "fastify";
+import { Question, User } from "@prisma/client";
+import { randomUUID } from "crypto";
+import fastify, { FastifyRequest } from "fastify";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify()
 
 app.get("/", (request, reply) => {
   reply.send(200)
 })
+
+interface IUserRequest {
+  name: string;
+  title?: string;
+  email: string;
+  password: string;
+}
+
+app.post("/users", async (request: FastifyRequest<{ Body: IUserRequest }>, reply) => {
+  const { name, title, email, password } = request.body;
+
+  const user: User = {
+    id: randomUUID(),
+    name,
+    title: (title ? title : null),
+    email,
+    password,
+    created_at: new Date()
+  }
+
+  try {
+    await prisma.user.create({ data: user })
+  }
+  catch (err) {
+    reply.code(400).send()
+    throw new Error()
+  }
+
+  return reply.code(201).send()
+})
+
