ab89a0bf2cca84e597c8717a14a15025c5a4c39b
Kauã Steliano <stelianok@gmail.com>
2024-01-08 14:14:12 -0300

    Removed updated_at field from users table

---
+++ prisma/migrations/20240108170200_removed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "updated_at";
