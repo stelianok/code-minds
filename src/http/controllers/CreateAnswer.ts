538f92dc617a0d0c95c53f690829c829d5cf864a
Kauã Steliano <stelianok@gmail.com>
2024-01-10 13:59:16 -0300

    Implemented CreateAnswerController

---
+++ src/http/controllers/CreateAnswer.ts
@@ -0,0 +1,42 @@
+import { ResourceNotFoundError } from "@/useCases/errors/ResourceNotFoundError";
+import { makeCreateAnswerUseCase } from "@/useCases/factories/makeCreateAnswerUseCase";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+interface ICreateAnswerRequest {
+  description: string;
+  author_id: string;
+  question_id: string;
+}
+
+export async function CreateAnswer(
+  request: FastifyRequest<{ Body: ICreateAnswerRequest }>,
+  reply: FastifyReply
+) {
+
+  const registerBodySchema = z.object({
+    description: z.string(),
+    author_id: z.string().uuid(),
+    question_id: z.string().uuid()
+  })
+
+  const { description, author_id, question_id } = registerBodySchema.parse(request.body);
+
+  try {
+    const createAnswerUseCase = makeCreateAnswerUseCase()
+
+    await createAnswerUseCase.execute({
+      description,
+      author_id,
+      question_id
+    });
+  }
+  catch (err) {
+    if (err instanceof ResourceNotFoundError) {
+      reply.status(409).send({ message: err.message })
+    }
+    throw err;
+  }
+
+  return reply.status(201).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { createUser } from './controllers/CreateUser';
 import { CreateQuestion } from './controllers/CreateQuestion';
+import { CreateAnswer } from './controllers/CreateAnswer';
 
 export async function AppRoutes(app: FastifyInstance) {
   app.get("/", (request, reply) => {
@@ -9,5 +10,5 @@ export async function AppRoutes(app: FastifyInstance) {
 
   app.post('/users', createUser)
   app.post('/questions', CreateQuestion)
-
+  app.post('/answers', CreateAnswer)
 }
