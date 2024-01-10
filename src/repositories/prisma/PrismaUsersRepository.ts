666a38a8ab2e3cbfe2b2b853af895e360804c160
Kauã Steliano <stelianok@gmail.com>
2024-01-10 11:54:45 -0300

    Created empty method to findById on Prisma repository.

--- src/repositories/prisma/PrismaUsersRepository.ts
+++ src/repositories/prisma/PrismaUsersRepository.ts
@@ -3,6 +3,9 @@ import { IUsersRepository } from "../IUsersRepository";
 import { prisma } from "@/lib/prisma";
 
 export class PrismaUsersRepository implements IUsersRepository {
+  findById(id: string): Promise<{ id: string; name: string; title: string | null; email: string; password: string; created_at: Date; } | null> {
+    throw new Error("Method not implemented.");
+  }
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({ data });
 
