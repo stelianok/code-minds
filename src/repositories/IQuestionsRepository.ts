55fbac2adc6b5cc8aaa186dd5f253e935bb4e262
Kauã Steliano <stelianok@gmail.com>
2024-01-10 11:39:27 -0300

    Implemented new findById method

--- src/repositories/IQuestionsRepository.ts
+++ src/repositories/IQuestionsRepository.ts
@@ -3,4 +3,5 @@ import { Prisma, Question } from "@prisma/client";
 export interface IQuestionsRepository {
   create(data: Prisma.QuestionCreateInput): Promise<Question>
   findByTitle(title: string): Promise<Question | null>
+  findById(id: string): Promise<Question | null>
 }
--- src/repositories/inMemory/InMemoryQuestionRepository.ts
+++ src/repositories/inMemory/InMemoryQuestionRepository.ts
@@ -1,16 +1,22 @@
 import { Prisma, Question } from "@prisma/client";
 import { IQuestionsRepository } from "../IQuestionsRepository";
 import { randomUUID } from "crypto";
+import { ResourceNotFoundError } from "@/useCases/errors/ResourceNotFoundError";
 
 export class InMemoryQuestionRepository implements IQuestionsRepository {
   public questions: Question[] = [];
 
   async create(data: Prisma.QuestionCreateInput) {
-    const question = {
+
+    if (!data.author.create?.id) {
+      throw new ResourceNotFoundError()
+    }
+
+    const question: Question = {
       id: data.id ?? randomUUID(),
       title: data.title,
       description: data.description,
-      author_id: 'user-01',
+      author_id: data.author.create.id,
       score: 0,
       created_at: new Date(),
       updated_at: new Date()
@@ -29,5 +35,15 @@ export class InMemoryQuestionRepository implements IQuestionsRepository {
     }
 
     return question;
+  }
+
+  async findById(id: string) {
+    const question = this.questions.find((question) => question.id === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
   }
 }
